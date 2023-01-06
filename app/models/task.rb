6cb31a92e3cd8b205d4470e71ebc9657d14a4a50
Arihant Hirawat <arihant2310@gmail.com>
2023-01-06 13:53:57 +0530

    add validations for task name, desc and status

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,7 @@
 class Task < ApplicationRecord
   enum status: { not_started: 0, in_progress: 1, blocked: 2, done: 3 }, _default: :not_started
+
+  validates :name, presence: true, length: { minimum: 3, maximum: 50 }
+  validates :description, length: { minimum: 10, maximum: 150 }
+  validates :status, inclusion: { in: statuses }
 end
