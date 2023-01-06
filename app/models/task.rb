15284cda90df4be840557557eff30da958e877ff
Arihant Hirawat <arihant2310@gmail.com>
2023-01-06 14:15:42 +0530

    add comments scaffold and associated with task

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,5 @@
 class Task < ApplicationRecord
+  has_many :comments
   enum status: { not_started: 0, in_progress: 1, blocked: 2, done: 3 }, _default: :not_started
 
   validates :name, presence: true, length: { minimum: 3, maximum: 50 }
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_06_065045) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_06_083820) do
+  create_table "comments", force: :cascade do |t|
+    t.text "content"
+    t.integer "task_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["task_id"], name: "index_comments_on_task_id"
+  end
+
   create_table "tasks", force: :cascade do |t|
     t.string "name"
     t.text "description"
@@ -20,4 +28,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_06_065045) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "comments", "tasks"
 end
