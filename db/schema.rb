710ff3e51914f87029343da3aee07ef5f2fea2e5
Arihant Hirawat <arihant2310@gmail.com>
2023-01-06 12:27:03 +0530

    add migration to change the task status type to integer

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_06_061431) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_06_065045) do
   create_table "tasks", force: :cascade do |t|
     t.string "name"
     t.text "description"
     t.datetime "due_date"
-    t.string "status"
+    t.integer "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
