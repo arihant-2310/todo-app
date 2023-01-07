42690e346edd80bfe66bec7747a8d8e0342017b6
Arihant Hirawat <arihant2310@gmail.com>
2023-01-07 09:43:06 +0530

    add user login devise gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  resources :comments
+  root 'pages#home'
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    omniauth_callbacks: 'users/omniauth_callbacks'
+  }
   resources :tasks
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
