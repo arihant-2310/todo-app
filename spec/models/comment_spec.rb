3aba18b1bd15247e12ba0cb71288538ae1b14eb7
Arihant Hirawat <arihant2310@gmail.com>
2023-01-09 20:05:07 +0530

    add test for comments association

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -6,4 +6,9 @@ describe Comment, type: :model do
       it { should validate_presence_of(:content) }
     end
   end
+
+  describe 'associations' do
+    it { should belong_to(:user).class_name('User') }
+    it { should belong_to(:task).class_name('Task') }
+  end
 end
