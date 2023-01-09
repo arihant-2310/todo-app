4c026937238426bc5ee57a4a27d083aa51013944
Arihant Hirawat <arihant2310@gmail.com>
2023-01-09 20:48:05 +0530

    add test for comments validations

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -2,13 +2,39 @@ require 'rails_helper'
 
 describe Comment, type: :model do
   describe 'Validations' do
-    context 'content' do
-      it { should validate_presence_of(:content) }
+    it { should validate_presence_of(:content) }
+
+    it 'is not valid if content length is less than 2 characters' do
+      comment = Comment.new(content: 'x', task: Task.new, user: User.new)
+
+      expect(comment).to_not be_valid
+      expect(comment.errors.full_messages.first).to eq 'Content is too short (minimum is 2 characters)'
+    end
+
+    it 'is not valid if content length is more than 150 characters' do
+      comment = Comment.new(content: 'x' * 151 , task: Task.new, user: User.new)
+
+      expect(comment).to_not be_valid
+      expect(comment.errors.full_messages.first).to eq 'Content is too long (maximum is 150 characters)'
+    end
+
+    it 'is not valid if user is not present' do
+      comment = Comment.new(content: 'x' * 151 , task: Task.new)
+
+      expect(comment).to_not be_valid
+      expect(comment.errors.full_messages.first).to eq 'User must exist'
+    end
+
+    it 'is not valid if task is not present' do
+      comment = Comment.new(content: 'x' * 151 , user: User.new)
+
+      expect(comment).to_not be_valid
+      expect(comment.errors.full_messages.first).to eq 'Task must exist'
     end
   end
 
   describe 'associations' do
-    it { should belong_to(:user).class_name('User') }
-    it { should belong_to(:task).class_name('Task') }
+    it { should belong_to(:user).class_name('User').required(true) }
+    it { should belong_to(:task).class_name('Task').required(true) }
   end
 end
