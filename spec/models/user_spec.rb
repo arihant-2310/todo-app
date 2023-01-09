697a764ad0d54984f790bdd9aca5cf149b6a85a7
Arihant Hirawat <arihant2310@gmail.com>
2023-01-09 22:45:31 +0530

    add specs for all_tasks in user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,4 +7,39 @@ describe User, type: :model do
     it { should have_many(:assignments).class_name('Assignment').dependent(:destroy)  }
     it { should have_many(:assigned_tasks).through(:assignments).source(:task) }
   end
+
+  describe 'all_tasks' do
+    it 'returns only created tasks' do
+      user = User.new(uid: 'uid', provider: 'google', email: 'random@random.com', password: 'password', full_name: 'name')
+      task = Task.create(user:, name: 'task1', description: 'description')
+
+      all_tasks = user.all_tasks
+
+      expect(all_tasks.size).to eq(1)
+      expect(all_tasks.first).to eq task
+    end
+
+    it 'returns only assigned tasks' do
+      user1 = User.new(uid: 'uid', provider: 'google', email: 'random@random.com', password: 'password', full_name: 'name')
+      user2 = User.new(uid: 'uid2', provider: 'google', email: 'random2@random.com', password: 'password', full_name: 'name2')
+
+      task = Task.create(user: user1, name: 'task1', description: 'description')
+      Assignment.create(user: user2, task:)
+      all_tasks = user2.all_tasks
+
+      expect(all_tasks.size).to eq(1)
+      expect(all_tasks.first).to eq task
+    end
+
+    it 'returns all unique created and assigned tasks' do
+      user = User.new(uid: 'uid', provider: 'google', email: 'random@random.com', password: 'password', full_name: 'name')
+
+      task = Task.create(user: user, name: 'task1', description: 'description')
+      Assignment.create(user: user, task:)
+      all_tasks = user.all_tasks
+
+      expect(all_tasks.size).to eq(1)
+      expect(all_tasks.first).to eq task
+    end
+  end
 end
