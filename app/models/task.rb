0322277ad2ce401003162415ee1046abe9e3e1e3
Arihant Hirawat <arihant2310@gmail.com>
2023-01-09 17:20:26 +0530

    add association between the task and its assignees

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,8 +1,8 @@
 class Task < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
-  has_many :users, through: :assignments, dependent: :destroy
   has_many :assignments, dependent: :destroy
+  has_many :assignees, through: :assignments, source: :task
   accepts_nested_attributes_for :assignments, allow_destroy: true, reject_if: :all_blank
 
   enum status: { not_started: 0, in_progress: 1, blocked: 2, done: 3 }, _default: :not_started
