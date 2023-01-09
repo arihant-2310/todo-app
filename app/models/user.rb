1dfecd09ecfaab16d86d64cc660844e77501c954
Arihant Hirawat <arihant2310@gmail.com>
2023-01-09 17:55:04 +0530

    fix source of task and user association

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_many :tasks, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :assignments, dependent: :destroy
-  has_many :assigned_tasks, through: :assignments, source: :user
+  has_many :assigned_tasks, through: :assignments, source: :task
 
   devise :database_authenticatable, :registerable,
          :rememberable, :validatable,
