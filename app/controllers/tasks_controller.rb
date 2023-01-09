3e4f9a457fc9e5c70704e8bcd543ae42b34c4a5b
Arihant Hirawat <arihant2310@gmail.com>
2023-01-09 18:47:38 +0530

    remove json format from task controller methods

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -22,36 +22,25 @@ class TasksController < ApplicationController
   def create
     @task = current_user.tasks.new(task_params)
 
-    respond_to do |format|
-      if @task.save
-        format.html { redirect_to task_url(@task), notice: 'Task was successfully created.' }
-        format.json { render :show, status: :created, location: @task }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @task.errors, status: :unprocessable_entity }
-      end
+    if @task.save
+      redirect_to task_url(@task), notice: 'Task was successfully created.'
+    else
+      render :new, status: :unprocessable_entity
     end
   end
 
   def update
-    respond_to do |format|
-      if @task.update(task_params)
-        format.html { redirect_to task_url(@task), notice: 'Task was successfully updated.' }
-        format.json { render :show, status: :ok, location: @task }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @task.errors, status: :unprocessable_entity }
-      end
+    if @task.update(task_params)
+      redirect_to task_url(@task), notice: 'Task was successfully updated.'
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
 
   def destroy
     @task.destroy
 
-    respond_to do |format|
-      format.html { redirect_to tasks_url, notice: 'Task was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to tasks_url, notice: 'Task was successfully destroyed.'
   end
 
   private
