7db360e48348255a66397ce0275474f158148763
Arihant Hirawat <arihant2310@gmail.com>
2023-01-09 21:16:03 +0530

    update comment model specs

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -3,34 +3,8 @@ require 'rails_helper'
 describe Comment, type: :model do
   describe 'Validations' do
     it { should validate_presence_of(:content) }
-
-    it 'is not valid if content length is less than 2 characters' do
-      comment = Comment.new(content: 'x', task: Task.new, user: User.new)
-
-      expect(comment).to_not be_valid
-      expect(comment.errors.full_messages.first).to eq 'Content is too short (minimum is 2 characters)'
-    end
-
-    it 'is not valid if content length is more than 150 characters' do
-      comment = Comment.new(content: 'x' * 151 , task: Task.new, user: User.new)
-
-      expect(comment).to_not be_valid
-      expect(comment.errors.full_messages.first).to eq 'Content is too long (maximum is 150 characters)'
-    end
-
-    it 'is not valid if user is not present' do
-      comment = Comment.new(content: 'x' * 151 , task: Task.new)
-
-      expect(comment).to_not be_valid
-      expect(comment.errors.full_messages.first).to eq 'User must exist'
-    end
-
-    it 'is not valid if task is not present' do
-      comment = Comment.new(content: 'x' * 151 , user: User.new)
-
-      expect(comment).to_not be_valid
-      expect(comment.errors.full_messages.first).to eq 'Task must exist'
-    end
+    it { should validate_length_of(:content).is_at_least(2) }
+    it { should validate_length_of(:content).is_at_most(150) }
   end
 
   describe 'associations' do
