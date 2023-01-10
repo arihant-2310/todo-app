7e0920a7c20ca26ae81b694e775147220e2c04bf
Arihant Hirawat <arihant2310@gmail.com>
2023-01-10 10:59:40 +0530

    add specs for test controller new action

--- spec/requests/tasks_controller_spec.rb
+++ spec/requests/tasks_controller_spec.rb
@@ -1,17 +1,47 @@
 require 'rails_helper'
 
 describe TasksController, type: :request do
-  describe 'GET /tasks' do
-    context 'with login' do
-      before do
-        user = User.new(uid: 'uid', provider: 'google', email: 'random@random.com', password: 'password', full_name: 'name')
-        sign_in user
-        get tasks_path
-      end
+  context 'with login' do
+    before do
+      user = User.new(uid: 'uid', provider: 'google', email: 'random@random.com', password: 'password', full_name: 'name')
+      sign_in user
+    end
 
+    describe 'GET /tasks' do
       it 'renders index template with a successful response' do
+        get tasks_path
+
         expect(response).to be_successful
         expect(response).to render_template :index
+      end
+    end
+
+    describe 'GET /tasks/new' do
+      it 'renders new template with a successful response' do
+        get new_task_path
+
+        expect(response).to be_successful
+        expect(response).to render_template :new
+      end
+    end
+  end
+
+  context 'without login' do
+    describe 'GET /tasks' do
+      it 'does not renders index template with a successful response' do
+        get tasks_path
+
+        expect(response).to_not be_successful
+        expect(response).to_not render_template :index
+      end
+    end
+
+    describe 'GET /tasks/new' do
+      it 'does not renders new template with a successful response' do
+        get new_task_path
+
+        expect(response).to_not be_successful
+        expect(response).to_not render_template :new
       end
     end
   end
