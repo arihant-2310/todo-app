5bf16580585f2f7f72128e4d086f4a7db85b80f7
Arihant Hirawat <arihant2310@gmail.com>
2023-01-10 09:43:16 +0530

    remove required true association which is default true

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ApplicationRecord
-  belongs_to :task, required: true
-  belongs_to :user, required: true
+  belongs_to :task
+  belongs_to :user
   validates :content, presence: true, length: { minimum:2, maximum: 150 }
 end
